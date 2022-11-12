8e2aed9951c6f16180368b37af9b0bf2d8bba404
chocolaggibbiddori <didrkd684@naver.com>
2022-11-12 21:23:45 +0900

    feat : http 요청 파라미터 - @RequestParam
    required = true : 필수 파라미터
    required = false : 없으면 null 입력

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -47,4 +47,14 @@ public class RequestParamController {
 
         return "ok";
     }
+
+    @ResponseBody
+    @RequestMapping("request-param-required")
+    public String requestParamRequired(@RequestParam(required = true) String username,
+                                       @RequestParam(required = false) Integer age) {
+
+        log.info("username = {}, age = {}", username, age);
+
+        return "ok";
+    }
 }
