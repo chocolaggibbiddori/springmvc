d476b93965475962846cc44657b927f51e4785ec
chocolaggibbiddori <didrkd684@naver.com>
2022-11-14 11:41:31 +0900

    refactor : http 요청 메시지 - JSON v4
    HttpEntity<HelloData>

--- src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
+++ src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
@@ -3,6 +3,7 @@ package hello.springmvc.basic.request;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +48,16 @@ public class RequestBodyJsonController {
     @ResponseBody
     @PostMapping("/request-body-json-v3")
     public String requestBodyJsonV3(@RequestBody HelloData helloData) {
+        log.info("username = {}, age = {}", helloData.getUsername(), helloData.getAge());
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v4")
+    public String requestBodyJsonV4(HttpEntity<HelloData> data) {
+        HelloData helloData = data.getBody();
+
         log.info("username = {}, age = {}", helloData.getUsername(), helloData.getAge());
 
         return "ok";
